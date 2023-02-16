661563db93e655206714d000dd03af1424738726
gayeongogo <rkdus1357@gmail.com>
2023-02-16 17:46:08 +0900

    draw canvas complete

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@
   event.target.style.background = 'linear-gradient(to right, #a465ce 0%, #a465ce '+gradient_value * event.target.value +'%, rgb(236, 236, 236) ' +gradient_value *  event.target.value + '%, rgb(236, 236, 236) 100%)';
 }); */
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext("2d");
 
 
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+
+//기본 드로잉
 ctx.strokeStyle = "#000000";
 ctx.lineWidth = 2.5;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 700, 700);
+
 
 let drawing = false;
 
@@ -21,8 +26,8 @@ function startDrawing() {
 };
 
 function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
+    let x = event.offsetX;
+    let y = event.offsetY;
 
     if(!drawing) {
         ctx.beginPath();
@@ -33,13 +38,85 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    drawing = true;
+
+
+//컬러 변경
+let colors = document.getElementsByClassName('controls-color');
+
+function colorClick(event) {
+    let color = event.target.style.background;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
+
+Array.from(colors).forEach((color) => {
+    color.addEventListener("click", colorClick)
+})
+
+//브러쉬 사이즈 변경
+let range = document.getElementById("range");
+
+function rangeChange(event) {
+    let brushSize = event.target.value;
+    ctx.lineWidth = brushSize;
+}
+
+if(range){
+    range.addEventListener("input", rangeChange);
+}
+
+//채우기
+let mode = document.getElementById('mode');
+let draw = false;
+let fill = false;
+
+function modeChange() {
+    if(fill === true) {
+        fill = false;
+        mode.innerText = "Fill"
+    }else{
+        fill = true;
+        mode.innerText = "Draw"
+    }
+}
+
+function canvasClick() {
+    if(fill) {
+        ctx.fillRect(0, 0, 700, 700)
+    }
+}
+
+if(mode){
+    mode.addEventListener("click", modeChange);
+}
+
+//저장
+let save = document.getElementById('save');
+
+function saved() {
+    let image = canvas.toDataURL("image/jpg");
+    let link = document.createElement('a');
+    link.href = image;
+    link.download = "drawing";
+    link.click()
+}
+
+function rightClick(event){
+
+    event.preventDefault();
+
+}
+
+if(save) {
+    save.addEventListener("click", saved)
+}
+
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startDrawing);
     canvas.addEventListener("mouseup", stopDrawing);
     canvas.addEventListener("mouseleave", stopDrawing);
+    canvas.addEventListener("click", canvasClick);
+    canvas.addEventListener("ontextmenu", rightClick);
 }
